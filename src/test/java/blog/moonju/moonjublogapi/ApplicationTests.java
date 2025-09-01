61e8f4f636ddd505df2d2ed108fd3df3e340596f
som141 <163431679+som141@users.noreply.github.com>
2025-09-01 16:55:03 +0900

    s

---
+++ src/test/java/blog/moonju/moonjublogapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.moonju.moonjublogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
