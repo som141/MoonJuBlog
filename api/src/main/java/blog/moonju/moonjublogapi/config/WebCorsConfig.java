59f50fbdc790d6d21077a3333827540155cc8156
som141 <163431679+som141@users.noreply.github.com>
2025-09-14 13:39:40 +0900

    d

--- api/src/main/java/blog/moonju/moonjublogapi/config/WebCorsConfig.java
+++ api/src/main/java/blog/moonju/moonjublogapi/config/WebCorsConfig.java
@@ -3,6 +3,7 @@ package blog.moonju.moonjublogapi.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebCorsConfig implements WebMvcConfigurer {
 
